70e7561414fe3e2c351da7f2dd48ec982318bedd
isoqjonmuminjonov-lgtm <isoqjonmuminjonov@gmail.com>
2026-02-14 15:52:44 +0500

    isoqjon

---
+++ isoqjon.script/script.js
@@ -0,0 +1,24 @@
+ //var name = "isoqjon";
+//console.log();
+//for(var i=0; i < 10; i++);{
+   // console.log(name +i);
+//}
+
+
+//var num = 5;
+//console.log(num);
+//num ++;
+//console.log(num);
+//num --;
+//console.log(num);
+
+//for (var i = 0; i <= 5 ; i++){
+  //  console.log(i);
+//}
+
+var ol = +prompt("nechchi marotaba alert chiqsin")
+ for(var i=0; i <ol ; i ++){
+  console.log(i);
+};  alert("++")
+
+
