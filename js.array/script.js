77da5e59a5adee17c0611d6d3fbb00d0b2df152b
isoqjonmuminjonov-lgtm <isoqjonmuminjonov@gmail.com>
2026-02-14 16:12:13 +0500

    first-commit

--- js.array/script.js
+++ js.array/script.js
@@ -3,7 +3,7 @@ var matn = prompt("so'z yoki gap kiriting").trim();
 var harf = prompt("xoxlagan harfingizni kiriting").trim()
 var isoq = 0;
 
-for (var i = 0; i < matn.length; i++) {
+for (var i = 0; i < matn.length; i++) { 
     if (matn[i].toLowerCase() == harf.toLowerCase()) {
         isoq++;
     }
