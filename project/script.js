1a7bc4443e55e1bc3ab0b6f421437c30508e66f4
isoqjonmuminjonov-lgtm <isoqjonmuminjonov@gmail.com>
2026-02-17 16:11:55 +0500

    harf soni

---
+++ project/script.js
@@ -0,0 +1,40 @@
+var isoq = prompt("son yozing yoki oy yozing").trim();{
+
+}if(isoq == febraure || isoq == 1>12    ){
+    console.log( isoq = "1");
+}else if(isoq == yanvar  ){
+    console.log( isoq = "1");
+}
+else if(isoq == febraure  ){
+    console.log( isoq = "2");
+}
+else if(isoq == mart  ){
+    console.log( isoq = "3");
+}
+else if(isoq == aprel  ){
+    console.log( isoq = "4");
+}
+else if(isoq == may  ){
+    console.log( isoq = "5");
+}
+else if(isoq == iyun  ){
+    console.log( isoq = "6");
+}
+else if(isoq == iyul  ){
+    console.log( isoq = "7");
+}
+else if(isoq == avgust  ){
+    console.log( isoq = "8");
+}
+else if(isoq == sentyabr  ){
+    console.log( isoq = "9");
+
+}else if(isoq == oktyabr  ){
+    console.log( isoq = "10");
+}
+else if(isoq == noyabr  ){
+    console.log( isoq = "11");
+}
+else if(isoq == dekabr  ){
+    console.log( isoq = "12");
+}
